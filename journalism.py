b0c95332f9f398742b6c8a7db1029d588ada95c7
Jordan Martel <jmartel@iu.edu>
2020-08-13 18:08:40 -0400

    MAINT

--- journalism.py
+++ journalism.py
@@ -1,160 +1,179 @@
-"""Journalism Equilibrium"""
-class JournalismEquilibrium:
-    """Equilibrium class for journalism model"""
+import numpy as np
 
-    def __init__(self,alpha,kappa,chi,b_bar,c_bar,v_bar):
-        """
-        Parameters
-        ----------
-        alpha : float
-            The journalist's skill
-        kappa : float
-            Convexity of the trading cost
-        chi : float
-            Mass of readers
-        b_bar : float
-            Highest permissible level of obfuscation
-        c_bar : float
-            Highest opportunity cost for the journalist
-        v_bar : float
-            Length of the support of firm values
-        """
+# =====================================================================
+# MODEL FUNCTIONS
 
-        # ---------------------------------------------------------------------
-        # parameters
+# ---------------------------------------------------------------------
+# equilibrium bias
+@np.vectorize
+def _b(v):
+	"""equilibrium bias"""
+	if v < -v_hat:
+		return b_hat
+	elif v < 0.:
+		return (1./(3.*v_hat))*delta*b_hat
+	elif v < 3*v_hat:
+		return (1./v_hat)*delta*b_hat
+	else: 
+		return b_hat
+# ---------------------------------------------------------------------
+# equilibrium reporting probability
+@np.vectorize
+def _pi_R(v):
+	"""equilibrium reporting probability"""
+	if v < -v_hat:
+		return K0*(delta**2.-v_hat**2.)
+	elif v < 0.:
+		return (8./9.)*K0*delta**2.
+	elif v < 3*v_hat:
+		return 0.
+	else: 
+		return K0*(delta**2.-v_hat**2.)
+# ---------------------------------------------------------------------
+# equilibrium report
+def _sigma_J(v):
+	"""equilibrium report"""
+	return v+(1.-alpha)*_b(v)
+# ---------------------------------------------------------------------
+# equilibrium reporting probability"""
+@np.vectorize
+def _pi_R(b):
+	"""equilibrium reporting probability"""
+	coef1 = (1.+2.*chi)/(2.*kappa*(1.+chi)**2.)
+	Delta_R = coef1*(delta**2.-((1.-alpha)*b)**2.)
+	if Delta_R < 0.:
+		return 0.
+	elif Delta_R < c_bar:
+		return Delta_R/c_bar
+	else:
+		return 1.
 
-        # organic parameters
-        self.alpha  = alpha
-        self.kappa  = kappa
-        self.chi    = chi
-        self.b_bar  = b_bar
-        self.c_bar  = c_bar
-        self.v_bar  = v_bar
+# ==============================================================================
+# VARIABLES AND VARIABLE PLOTTING
 
-        # synthetic parameters
-        self.mu_v   = v_bar/2. 
-        self.v_hat  = (1.-alpha)*b_bar
-        self.K0     = (1.+2.*chi)/(2.*kappa*c_bar*(1.+chi)**2.)
-        b_bar_max   = self.mu_v/(3.*(1.-alpha))
-        c_bar_min   = 16.*(1.+2.*chi)*self.mu_v**2./(9.*kappa*(1.+chi)**2.)
-        
-        # ---------------------------------------------------------------------
-        # parameter checks
+class Variable:
+	"""Variable class"""
 
-        if alpha > 1. or alpha < 0.:
-            raise Exception('alpha > 1. or alpha < 0.')
+	def __init__(self,name,ran,labels,ticks,ticklabels):
+		"""
+		Parameters
+		----------
+		ran : (float, float)
+			Variable's min and max values resp.
+		ticks : (list of) float
+			x ticks
+		ticklabs : (list of) str
+			x tick labels
+		"""		
 
-        if kappa < 0.:
-            raise Exception('kappa < 0.')
+def plot(x,Y,linespecs=['-'],colors=['k'],linewidths=[1]):
+	"""
+	For each y in Y, plots y against x
 
-        if chi < 0.:
-            raise Exception('chi < 0.')
+	Parameters
+	----------
+	x, Y : Variable, (list of) Variables
+		Variables to plot
 
-        if b_bar > b_bar_max:
-            raise Exception('b_bar > b_bar_max')
+	Notes
+	-----
+	Default behavior is to cycle through linespecs and colors
+	"""		
+	if not len(linespecs) is len(colors) is len(linewidths) is len(Y):
+		raise Exception('PC load letter')
 
-        if c_bar < c_bar_min:
-            raise Exception('c_bar < c_bar_min')
+	for y in Y:
+		plt.plot(x._plt,y._plt)	
+	plt.xlabel(x.name)
 
-        # ---------------------------------------------------------------------
-        # parameters
-        self.model_parameters = []
+	if len(Y) > 1:
+		plt.legend([y.name for y in Y])
+	else:
+		plt.ylabel(Y[0].name)
 
-        firm_value  = ParVar(
-            r'$0$',
-            r'$\mu_{v}-\widehat{v}$',
-            r'$\mu_{v}$',
-            r'$\mu_{v}+3\widehat{v}$',
-            r'$\overline{v}$'
-        )
+	fname_x = x.name.replace(' ','_')
+	fname_y = '_'.join([y.name.replace(' ','_') for y in Y])
+	plt.savefig(fname_x + '_' + fname_y + '.pdf')
 
-        firm_bias   = ParVar(
-            r'$0$',
-            r'$\\sup_{\\Delta_{R}}^{-1}(\\overline{c})$',
-            r'$\\sup_{\\Delta_{R}}^{-1}(0)$'
-        )
+# ==============================================================================
+# EQUILIBRIUM
 
-        # ---------------------------------------------------------------------
-        # objects
-        self.model_objects = []
+class Equilibrium:
+	"""Equilibrium class for journalism model"""
 
-        # ---------------------------------------------------------------------
-        # equilibrium reporting probability"""
-        @np.vectorize
-        def _pi_R(b):
-            """equilibrium reporting probability"""
-            coef1 = (1.+2.*chi)/(2.*kappa*(1.+chi)**2.)
-            Delta_R = coef1*((v-mu_v)**2.-((1.-alpha)*b)**2.
-            if Delta_R < 0.:
-                return 0.
-            elif Delta_R < c_bar:
-                return Delta_R/c_bar
-            else:
-                return 1.
+	def __init__(self,alpha,kappa,chi,b_bar,c_bar,v_bar):
+		"""
+		Parameters
+		----------
+		alpha : float
+			The journalist's skill
+		kappa : float
+			Convexity of the trading cost
+		chi : float
+			Mass of readers
+		b_bar : float
+			Highest permissible level of obfuscation
+		c_bar : float
+			Highest opportunity cost for the journalist
+		v_bar : float
+			Length of the support of firm values
+		"""
 
-        self.model_objects.append(Object(
-            name = 'equilibrium reporting probability',
-            func = _pi_R,
-            ylab = r'reporting probability $\pi_{R}^{*}$',
-            1.,
-            r'$1$'
-            ))
+		# ----------------------------------------------------------------------
+		# parameters
 
-        # ---------------------------------------------------------------------
-        # equilibrium bias
-        @np.vectorize
-        def _b(v):
-            """equilibrium bias"""
-            if v < -v_hat:
-                return b_hat
-            elif v < 0.:
-                return (1./(3.*v_hat))*(v-mu_v)*b_hat
-            elif v < 3*v_hat:
-                return (1./v_hat)*(v-mu_v)*b_hat
-            else: 
-                return b_hat
+		# organic parameters
+		self.alpha  = alpha
+		self.kappa  = kappa
+		self.chi    = chi
+		self.b_bar  = b_bar
+		self.c_bar  = c_bar
+		self.v_bar  = v_bar
 
-        self.model_objects.append(Object(
-            name = 'equilibrium bias',
-            func = _b,
-            ylab = r'bias $\b^{*}$',
-            b_bar,
-            r'$\overline{\b}$'
-            ))
+		# synthetic parameters
+		self.mu_v   = v_bar/2. 
+		self.v_hat  = (1.-alpha)*b_bar
+		self.K0     = (1.+2.*chi)/(2.*kappa*c_bar*(1.+chi)**2.)
+		b_bar_max   = self.mu_v/(3.*(1.-alpha))
+		c_bar_min   = 16.*(1.+2.*chi)*self.mu_v**2./(9.*kappa*(1.+chi)**2.)
+		
+		# parameter checks
+		if alpha > 1. or alpha < 0.:
+			raise Exception('alpha > 1. or alpha < 0.')
+		if kappa < 0.:
+			raise Exception('kappa < 0.')
+		if chi < 0.:
+			raise Exception('chi < 0.')
+		if b_bar > b_bar_max:
+			raise Exception('b_bar > b_bar_max')
+		if c_bar < c_bar_min:
+			raise Exception('c_bar < c_bar_min')
 
-        # ---------------------------------------------------------------------
-        # equilibrium reporting probability
-        @np.vectorize
-        def _pi_R(v):
-            """equilibrium reporting probability"""
-            if v < -v_hat:
-                return K0*((v-mu_v)**2.-v_hat**2.)
-            elif v < 0.:
-                return (8./9.)*K0*(v-mu_v)**2.
-            elif v < 3*v_hat:
-                return 0.
-            else: 
-                return K0*((v-mu_v)**2.-v_hat**2.)
+		# ----------------------------------------------------------------------
+		# equilibrium bias
+		@np.vectorize
+		_b(v):
+		name = 'equilibrium bias'
+		ylab = r'bias $\b^{*}$'
+		self._b = np.vectorize(_b)
 
-        self.model_objects.append(Object(
-            name = 'equilibrium reporting probability',
-            func = _pi_R,
-            ylab = r'reporting probability $\pi_{R}^{*}$',
-            1.,
-            r'$1$'
-            ))
+		# ----------------------------------------------------------------------
+		# equilibrium reporting probability
+		@np.vectorize
+		_pi_R(v):
+		name = 'equilibrium reporting probability'
+		ylab = r'reporting probability $\pi_{R}^{*}$'
+		self._pi_R = np.vectorize(_pi_R)
 
-        # ---------------------------------------------------------------------
-        # equilibrium report
-        @np.vectorize
-        def _sigma_J(v):
-            """equilibrium report"""
-            return v+(1.-alpha)*_b(v)
+		# ----------------------------------------------------------------------
+		# equilibrium report
+		_sigma_J(v):
+		name = 'equilibrium report'
+		ylab = r'report $s_{J}^{*}$'
 
-        self.model_objects.append(Object(
-            name = 'equilibrium report',
-            func = _sigma_J,
-            ylab = r'report $s_{J}^{*}$',
-            v_bar+(1.-alpha)*b_bar,
-            r'$\overline{v}+(1-\alpha)\overline{\b}$'
-            ))
+		# ----------------------------------------------------------------------
+		# equilibrium reporting probability"""
+		_pi_R(b):
+		name = 'equilibrium reporting probability'
+		ylab = r'reporting probability $\pi_{R}^{*}$'
+		self._pi_R = np.vectorize(_pi_R)
--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,12 @@
-def func(**kwarg):
-    kwarg
-    z = x + y
-    return z
+from journalism import *
+
+e = Equilibrium(
+	alpha = 1./2.,
+	kappa = 2./3.,
+	chi   = 1./2.,
+	b_bar = 4./3.,
+	c_bar = 10.,
+	v_bar = 4.
+)
+
+plot(e.v,[e.b])
